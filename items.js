541a96d8eaf6431b386f2c7b5ab4b6fa49d37120
Joshini9 <joshinigajula9@gmail.com>
2025-06-09 15:27:09 +0530

    Update items.js

--- items.js
+++ items.js
@@ -1,3 +1,10 @@
+  const menuToggle = document.getElementById("menuToggle");
+  const navLinks = document.getElementById("navLinks");
+
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+
 const items = [
   {
     id: "wallet",
